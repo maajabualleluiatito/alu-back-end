a96a776bcd129e2372905e6675846c614efb9b19
maajabualleluiatito <169038601+maajabualleluiatito@users.noreply.github.com>
2024-10-28 11:11:31 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""
-    python script that exports data in the JSON format
-"""
+"""Script to use a REST API, returns information about
+all tasks from all employees and export in JSON"""
 import json
 import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-    """
-        export to JSON
-    """
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+if __name__ == "__main__":
+    API_URL = "https://jsonplaceholder.typicode.com"
+
+    users = requests.get(f"{API_URL}/users").json()
+
+    dict_users_tasks = {}
+    for user in users:
+        tasks = requests.get(f"{API_URL}/users/{user['id']}/todos").json()
+
+        dict_users_tasks[user["id"]] = []
+        for task in tasks:
+            task_dict = {
+                "username": user["username"],
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            dict_users_tasks[user["id"]].append(task_dict)
+
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(dict_users_tasks, file)
